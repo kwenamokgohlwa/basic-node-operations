3f32b0cce674f37d64ea8ff12e6d9ccddbf8170b
Kwena Mokgohlwa <kwena@Kwenas-MacBook-Air.local>
2018-03-29 15:55:06 +0200

    Assignment Basic Node Operations Complete

--- commands.js
+++ commands.js
@@ -42,36 +42,68 @@ const commandLibrary = {
   // Usage: $ head [# of lines] [filename]
 
   "head": function(userInput) {
-    var n = userInput[0];
-    var fileName = userInput[1];
-    fs.readFile(fileName, (err, data) => {
-      if (err)  throw err;
-      var dataArr = data.toString().split("\n");
-      var nData = [];
-      for(var i = 0; i < n; i++) {
-        nData.push(dataArr[i]);
-      }
-      data = nData.join("\n");
-      done(data);
-    });
+    if (isNaN(userInput[0])){
+      var n = 10;
+      var fileName = userInput[0];
+      fs.readFile(fileName, (err, data) => {
+        if (err)  throw err;
+        var dataArr = data.toString().split("\n");
+        var nData = [];
+        for(var i = 0; i < n; i++) {
+          nData.push(dataArr[i]);
+        }
+        data = nData.join("\n");
+        done(data);
+      });
+    }else {
+      var n = userInput[0];
+      var fileName = userInput[1];
+      fs.readFile(fileName, (err, data) => {
+        if (err)  throw err;
+        var dataArr = data.toString().split("\n");
+        var nData = [];
+        for(var i = 0; i < n; i++) {
+          nData.push(dataArr[i]);
+        }
+        data = nData.join("\n");
+        done(data);
+      });
+    }
+
   },
 
   // Usage: $ tail [# of lines] [filename]
 
   "tail": function(userInput) {
-    var n = userInput[0];
-    var fileName = userInput[1];
-    fs.readFile(fileName, (err, data) => {
-      if (err)  throw err;
-      var dataArr = data.toString().split("\n");
-      n = dataArr.length - n;
-      var nData = [];
-      for(var i = n; i < dataArr.length; i++) {
-        nData.push(dataArr[i]);
-      }
-      data = nData.join("\n");
-      done(data);
-    });
+    if (isNaN(userInput[0])) {
+      var n = 10;
+      var fileName = userInput[0];
+      fs.readFile(fileName, (err, data) => {
+        if (err)  throw err;
+        var dataArr = data.toString().split("\n");
+        n = dataArr.length - n;
+        var nData = [];
+        for(var i = n; i < dataArr.length; i++) {
+          nData.push(dataArr[i]);
+        }
+        data = nData.join("\n");
+        done(data);
+      });
+    }else {
+      var n = userInput[0];
+      var fileName = userInput[1];
+      fs.readFile(fileName, (err, data) => {
+        if (err)  throw err;
+        var dataArr = data.toString().split("\n");
+        n = dataArr.length - n;
+        var nData = [];
+        for(var i = n; i < dataArr.length; i++) {
+          nData.push(dataArr[i]);
+        }
+        data = nData.join("\n");
+        done(data);
+      });
+    }
   },
 
   "errorHandler": function(userInput) {
